630e941ded6735c1cd65ac5d01c7ef501f13aba5
Lucio_Beckler <luciobecklerp@gmail.com>
2023-10-01 17:32:20 -0300

    Adicionando funcionalidade de cadastro de novos restaurantes usando funções assíncronas

--- Front/src/pages/Administracao/Restaurante/CadastraRestaurantes.tsx
+++ Front/src/pages/Administracao/Restaurante/CadastraRestaurantes.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import { Button, TextField } from '@mui/material';
+import axios from 'axios';
+import { error } from 'console';
+import React, { useState } from 'react';
 
 export default function CadastraRestaurantes() {
-    return <form></form>;
+    const [nomeRestaurante, setNomeRestaurante] = useState('');
+
+    async function aoSubmeterFORM(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log(nomeRestaurante);
+        try {
+            const response = await axios.post(
+                'http://0.0.0.0:8000/api/v2/restaurantes/',
+                { nome: nomeRestaurante }
+            );
+            setNomeRestaurante('');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <form onSubmit={aoSubmeterFORM}>
+            <TextField
+                value={nomeRestaurante}
+                onChange={(event) => {
+                    setNomeRestaurante(event.target.value);
+                }}
+                id='standard-basic'
+                label='Standard'
+                variant='standard'
+            />
+            <Button type='submit' variant='outlined'>
+                Outlined
+            </Button>
+        </form>
+    );
 }
